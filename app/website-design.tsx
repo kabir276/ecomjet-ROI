587871e373c2b024610d1e435925b409a28d10e5
kabir chhabra <kchhabra276@gmail.com>
2024-05-11 06:52:09 +0530

    Update website-design.tsx

--- app/website-design.tsx
+++ app/website-design.tsx
@@ -7,7 +7,7 @@ const WebsiteDesign = () => {
         <div className="text-white">
             <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
                 <div className="text-4xl  md:text-7xl text-center bg-clip-text text-transparent bg-gradient-to-b from-purple-500 to-[#fdfaff] bg-opacity-50">
-                    Website Design <br /> that works
+                   Products turned <br /> into Brands
                 </div>
                 <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto px-4">
                     Creating, designing and developing websites that work for your business.
