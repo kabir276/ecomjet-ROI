e1c2e718b484c92c459974407a5add9c68512836
kabir276 <kchhabra276@gmail.com>
2024-05-20 22:31:45 -0700

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -226,27 +226,25 @@ export default function Home() {
             </svg>
           </div>
         </button>
-
-        <div className="flex flex-row justify-center gap-1">
+        <div className="flex flex-row justify-center gap-1 mt-10">
           <div className="w-18 flex flex-col">
-            <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9"alt="" />
-            <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-xs text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
-          
-           &quot; They helped us scale dramatically while maintaining positive ROI. &quot;
+            <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9" alt="" />
+            <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-sm text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
+
+              &quot; They helped us scale dramatically while maintaining positive ROI. &quot;
             </p>
           </div>
-          <div className="w-18 flex flex-col">
-          <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9"alt="" />
-
-            <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-xs text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
-            &quot;They were very organized in terms of planning, documentation, and project management. &quot;
+          <div className="w-18 md:flex md:flex-col hidden ">
+            <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9" alt="" />
+            <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-sm text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
+              &quot;They were very organized in terms of planning, documentation, and project management. &quot;
             </p>
           </div>
-          <div className="w-18 flex flex-col">
-          <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9"alt="" />
+          <div className="w-18 md:flex md:flex-col hidden">
+            <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9" alt="" />
 
-            <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-xs text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
-            &quot; Their team was extremely friendly and responsive throughout. &quot;
+            <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-sm text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
+              &quot; Their team was extremely friendly and responsive throughout. &quot;
             </p>
           </div>
 
