efdd739c5e05c6e5fb284eb73a321b54cfbac514
kabir276 <kchhabra276@gmail.com>
2024-05-20 22:40:53 -0700

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
       </div>
 
       <Spotlight className="  md:flex md:-top-80 top-54  md:left-80 left-52 -top-56 " fill="rgba(151,106,242,0.64)" />
-      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-14 px-2">
+      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
         <div
           className="text-[32px] pb-5 md:text-7xl px-0 text-center  bg-clip-text text-transparent 
              bg-gradient-to-b from-neutral-50
@@ -226,7 +226,7 @@ export default function Home() {
             </svg>
           </div>
         </button>
-        <div className="flex flex-row justify-center gap-1 mt-10">
+        <div className="flex flex-row justify-center gap-1 mt-16">
           <div className="w-18 flex flex-col">
             <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9" alt="" />
             <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-sm text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
@@ -234,7 +234,7 @@ export default function Home() {
               &quot; They helped us scale dramatically while maintaining positive ROI. &quot;
             </p>
           </div>
-          <div className="w-18 md:flex md:flex-col hidden ">
+          <div className="w-18 md:flex md:flex-col hidden  md:-mb-4">
             <img src="/images/rating (2).png" className="h-16 w-16 m-auto -mb-9" alt="" />
             <p className="md:mt-4 mt-[5px]   md:w-auto w-[100%]  md:text-sm text-[14px]  font-normal  text-neutral-300 md:max-w-[20rem] max-w-[14rem] text-center mx-auto p-4">
               &quot;They were very organized in terms of planning, documentation, and project management. &quot;
