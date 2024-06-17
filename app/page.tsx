c77f76e31e85f71c23c9e66880740d8bb74a0f88
kabir276 <kchhabra276@gmail.com>
2024-06-17 07:20:16 -0700

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-import { useState } from "react";
-import Navbar from "@/components/navbar";
+"use client"
 import SliderOne from "@/components/ui/slider";
 import { Spotlight } from "@/components/ui/spotlight";
 import Image from "next/image";
 import Link from "next/link";
-
 import WebsiteDesign from "./website-design";
 import Services from "./services";
 import FAQS from "./faq";
@@ -16,28 +13,21 @@ import { motion } from "framer-motion";
 import { ThreeDCardExample } from "./snippets/3d-card-snippet";
 import { EvervaultCardSnippet } from "./snippets/evervault-card-snippet";
 import CalendlyWidget from "../components/calendly"
-import Ebook from "@/components/ebook";
+import { useRouter } from "next/navigation"
+
 export default function Home() {
-
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-
+  const router = useRouter();
   const scrollFunc = () => {
     const section = document.getElementById("scheduleCall");
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
   }
-
+  
   const websiteDesignRef = useRef<HTMLDivElement>(null);
-
-  const brandsRef = useRef<HTMLDivElement>(null);
-
-
-
-
-
-
-
+const redirectToEbook=()=>{
+  router.push("/ebook")
+}
   return (
     <div className="w-full  md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <div className="p-6 md:p-10 md:pl-16 md:pr-16 flex items-center justify-between z-50">
@@ -539,7 +529,133 @@ export default function Home() {
         </div> */}
        
         <Services />
-        <Ebook />
+        <div>
+            <div className="flex md:flex-row flex-col-reverse justify-center md:p-24 m-auto">
+                <div className="px-4">
+                    <div
+                        className="text-[20px] md:pb-5 md:text-2xl px-0 md:text-left text-center  bg-clip-text text-transparent bg-gradient-to-b from-[rgba(151,106,242)] to bg-[rgba(151,106,242,0.63)] bg-opacity-50"
+                    >
+                        ULTIMATE SAAS GUIDE
+                    </div>
+                    <div
+                        className="text-[40px] pb-5 md:text-5xl px-0 md:text-left text-center  bg-clip-text text-transparent bg-[#fff]"
+                    >
+                        The SaaS Marketing  <br />
+                        Playbook
+                    </div>
+                    <p className="md:mt-4 mt-[5px] text-center md:text-left  m-auto md:w-auto w-[100%]  md:text-[15px] text-[18px]  font-normal  text-neutral-300 md:max-w-[34rem] max-w-[24rem]">
+                        Ignite explosive growth for your SaaS venture with strategies from an industry insider&apos;s proven framework.
+                    </p>
+                    <div >
+                      <button onClick={()=>redirectToEbook()} className="button-str font-bold mt-[2.4rem] " style={{borderRadius:"10px" , padding: "10px 20px" }}>
+                                   
+                                        Get your access
+
+                                        <div className="star-1">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlSpace="preserve"
+                                                version="1.1"
+                                                style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: "crisp-edges", fillRule: 'evenodd', clipRule: 'evenodd' }}
+                                                viewBox="0 0 784.11 815.53"
+                                                xmlnsXlink="http://www.w3.org/1999/xlink"
+                                            >
+                                                <defs></defs>
+                                                <g id="Layer_x0020_1">
+                                                    <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                                                    <path
+                                                        className="fil0"
+                                                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                                                    ></path>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div className="star-2">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlSpace="preserve"
+                                                version="1.1"
+                                                style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: "crisp-edges", fillRule: 'evenodd', clipRule: 'evenodd' }}
+                                                viewBox="0 0 784.11 815.53"
+                                                xmlnsXlink="http://www.w3.org/1999/xlink"
+                                            >
+                                                <defs></defs>
+                                                <g id="Layer_x0020_1">
+                                                    <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                                                    <path
+                                                        className="fil0"
+                                                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                                                    ></path>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div className="star-3">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlSpace="preserve"
+                                                version="1.1"
+                                                style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: "crisp-edges", fillRule: 'evenodd', clipRule: 'evenodd' }}
+                                                viewBox="0 0 784.11 815.53"
+                                                xmlnsXlink="http://www.w3.org/1999/xlink"
+                                            >
+                                                <defs></defs>
+                                                <g id="Layer_x0020_1">
+                                                    <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                                                    <path
+                                                        className="fil0"
+                                                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                                                    ></path>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div className="star-4">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlSpace="preserve"
+                                                version="1.1"
+                                                style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: "crisp-edges", fillRule: 'evenodd', clipRule: 'evenodd' }}
+                                                viewBox="0 0 784.11 815.53"
+                                                xmlnsXlink="http://www.w3.org/1999/xlink"
+                                            >
+                                                <defs></defs>
+                                                <g id="Layer_x0020_1">
+                                                    <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                                                    <path
+                                                        className="fil0"
+                                                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                                                    ></path>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div className="star-5">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlSpace="preserve"
+                                                version="1.1"
+                                                style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: "crisp-edges", fillRule: 'evenodd', clipRule: 'evenodd' }}
+                                                viewBox="0 0 784.11 815.53"
+                                                xmlnsXlink="http://www.w3.org/1999/xlink"
+                                            >
+                                                <defs></defs>
+                                                <g id="Layer_x0020_1">
+                                                    <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                                                    <path
+                                                        className="fil0"
+                                                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                                                    ></path>
+                                                </g>
+                                            </svg>
+                                        </div>
+                      </button>
+                    </div>
+                </div>
+                <div>
+                    <img src="/images/Black & Purple Marketing Ebook Mockup Instagram Post.png" alt="" className="w-[33rem] h-[auto] m-auto md:-mt-28" />
+                </div>
+
+            </div>
+        </div>
+
         <div className="" id="scheduleCall" style={{ background: '' }}>
           <div className="flex lg:flex-row flex-col-reverse justify-center md:gap-24 md:p-12  px-8 lg:-mt-[5rem] ">
             <div className="calandly md:w-[50%] overflow:hidden">
